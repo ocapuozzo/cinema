6293f68df2a68770b077f9f8d9d9f834b8a4c875
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2018-11-07 14:49:05 +0100

    utf-8 dans index.jsp

--- src/main/java/fr/laerce/cinema/Liste.java
+++ src/main/java/fr/laerce/cinema/Liste.java
@@ -12,34 +12,36 @@ import java.util.Collections;
  * Created by fred on 03/02/2016.
  */
 public class Liste extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)  {
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 
+  }
+
+  protected void doGet(HttpServletRequest request,
+                       HttpServletResponse response) throws IOException {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>Liste des films</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>Les films</h1>");
+    out.println("<ul>");
+
+    // boucler sur les films
+    FilmsDonnees fd = new FilmsDonnees();
+    Collections.sort(fd.lesFilms, new FilmComparator());
+    for (Film film : fd.lesFilms) {
+      out.println("  <li style='height:60px;width:200px'>");
+      out.println("<a href='detail?id="+film.id+"'>" + film.titre + "</a>");
+     // +  "<img style='margin-left:20px;height:40px;width:40px' src='affiche?id="+film.id+"'/>");
+      out.println("  </li>");
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Liste des films</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Les films</h1>");
-        out.println("<ul>");
+    out.println("</ul>");
+    out.println("</body>");
+    out.println("</html>");
 
-        // boucler sur les films
-        FilmsDonnees fd = new FilmsDonnees();
-        Collections.sort(fd.lesFilms, new FilmComparator());
-        for (Film film : fd.lesFilms) {
-          out.println("  <li>");
-          out.println(film.titre);
-          out.println("  </li>");
-        }
-
-        out.println("</ul>");
-        out.println("</body>");
-        out.println("</html>");
-
-    }
+  }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,8 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 </head>
 <body>
 <h2>Le cinema de Tomcat</h2>
+<p><a href="liste"> Liste des films triés sur le titre</a> </p>
 </body>
 </html>
