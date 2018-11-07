dd562d71f340c622ca270786a6fcb1e4776289e0
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2018-11-07 11:43:16 +0100

    exercice 1.1 (remove import=

--- src/main/java/fr/laerce/cinema/Liste.java
+++ src/main/java/fr/laerce/cinema/Liste.java
@@ -2,7 +2,6 @@ package fr.laerce.cinema;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,13 +10,12 @@ import java.io.*;
 /**
  * Created by fred on 03/02/2016.
  */
-@WebServlet(name = "Affiche")
 public class Liste extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)  {
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<!DOCTYPE html>");
